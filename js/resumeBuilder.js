799631eff0d7e3c822b5b40eb355eb255d27962d
rachelenriquez <rachelenriquez@icloud.com>
2016-12-10 19:00:26 -0800

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,273 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Rachel<br>Enriquez",
+    "role": "Full Stack Web Developer",
+    "contacts": {
+        "mobile": "111-222-3333",
+        "email": "myemail@email.com",
+        "github": "rachelenriquez",
+        "twitter": "@rachelenriquez",
+        "location": "Corona, CA"
+    },
+    "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "skills": [
+        "HTML5",
+        "CSS",
+        "Javascript",
+        "PHP",
+        "Photoshop"
+    ],
+    "skillsValue": [
+        "90",
+        "70",
+        "50",
+        "30",
+        "70"
+    ],
+    "biopic": "https://unsplash.it/200/200"
+};
+
+var education = {
+    "schools": [{
+        "name": "California State Polytechnic University",
+        "location": "Pomona, CA",
+        "degree": "Bachelor of Science",
+        "majors": [
+            "Business Administration"
+        ],
+        "dates": "2009-2014",
+        "url": "http://www.csupomona.edu"
+    }, {
+        "name": "California State Polytechnic University",
+        "location": "Pomona, CA",
+        "degree": "Bachelor of Science",
+        "majors": [
+            "Business Administration"
+        ],
+        "dates": "2009-2014",
+        "url": "http://www.csupomona.edu"
+    }],
+    "onlineCourses": [{
+        "title": "Front End Developer Nanodegree",
+        "school": "Udacity",
+        "dates": "2016-current",
+        "url": "http://www.udacity.com"
+    }, {
+        "title": "Javascript Road Trip",
+        "school": "CodeSchool",
+        "dates": "2016",
+        "url": "http://www.codeschool.com"
+    }, {
+        "title": "jQuery",
+        "school": "CodeSchool",
+        "dates": "2016",
+        "url": "http://www.codeschool.com"
+    }]
+};
+
+var work = {
+    "jobs": [{
+        "employer": "Company Name",
+        "title": "Lorem Ipsum",
+        "location": "Rancho Cucamonga, CA",
+        "dates": "2012-2015",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }, {
+        "employer": "Company Name",
+        "title": "Lorem Ipsum",
+        "location": "Corona, CA",
+        "dates": "2016-current",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }, {
+        "employer": "Company Name",
+        "title": "Lorem Ipsum",
+        "location": "Corona, CA",
+        "dates": "2016-current",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }, {
+        "employer": "Company Name",
+        "title": "Lorem Ipsum",
+        "location": "Los Angeles, CA",
+        "dates": "2016-current",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }, {
+        "employer": "Company Name",
+        "title": "Owner",
+        "location": "Van Nuys, CA",
+        "dates": "2016-current",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }, {
+        "employer": "Company Name",
+        "title": "Owner",
+        "location": "Las Vegas, NV",
+        "dates": "2016-current",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "url": ""
+    }]
+};
+
+var projects = {
+    "projects": [{
+        "title": "Animal Trading Cards",
+        "dates": "November 2016",
+        "description": "Project for Udacity",
+        "images": [
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+        ],
+        "url": "http://www.udacity.com"
+    }, {
+        "title": "Animal Trading Cards",
+        "dates": "November 2016",
+        "description": "Project for Udacity",
+        "images": [
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+        ],
+        "url": "http://www.udacity.com"
+    }, {
+        "title": "Animal Trading Cards",
+        "dates": "November 2016",
+        "description": "Project for Udacity",
+        "images": [
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+        ],
+        "url": "http://www.udacity.com"
+    }, {
+        "title": "Responsive Blog",
+        "dates": "December 2016",
+        "description": "Project for Udacity",
+        "images": [
+            "https://unsplash.it/210/210",
+            "https://unsplash.it/210/210",
+        ],
+        "url": "http://www.udacity.com"
+    }]
+};
+
+bio.display = function() {
+    var formatBio = [
+        [HTMLbioPic, "biopic"],
+        [HTMLheaderName, "name"],
+        [HTMLheaderRole, "role"],
+        [HTMLwelcomeMsg, "welcomeMessage"]
+    ];
+    var formatContacts = [
+        [HTMLlocation, "location"],
+        [HTMLmobile, "mobile"],
+        [HTMLemail, "email"],
+        [HTMLtwitter, "twitter"],
+        [HTMLgithub, "github"]
+    ];
+    var formattedBio = '';
+    $.each(formatBio, function(k, v) {
+        formattedBio += formatBio[k][0].replace('%data%', bio[formatBio[k][1]]);
+    });
+    $('.aside').prepend(formattedBio);
+    /*
+
+      for (var j=0, t=formatBio.length; j<t; j++) {
+          formattedBio += formatBio[j][0].replace('%data%', bio[formatBio[j][1]]);
+      };
+      $('.aside').prepend(formattedBio);*/
+
+    var formattedContacts = '';
+    $.each(formatContacts, function(k, v) {
+        formattedContacts += formatContacts[k][0].replace('%data%', bio["contacts"][formatContacts[k][1]]);
+    });
+    $('#topContacts').append(formattedContacts);
+
+    $('#skills').prepend(HTMLskillsStart);
+    var formattedSkills = '';
+    $.each(bio.skills, function(k, v) {
+        formattedSkills += HTMLskills.replace('%data%', bio.skills[k]);
+        formattedSkills += HTMLskillsValue.replace('%data%', bio.skillsValue[k]);
+    });
+    $('.skillsContainer').append(formattedSkills);
+    $('#footerContacts').append(formattedContacts);
+}();
+
+projects.display = function() {
+    var formatProjects = [
+        [HTMLprojectTitle, "title"],
+        [HTMLprojectDates, "dates"],
+        [HTMLprojectDescription, "description"],
+    ];
+    $.each(projects.projects, function(k, v) {
+        var formattedProjects = '';
+        $('#projects').append(HTMLprojectStart);
+        $.each(formatProjects, function(j, x) {
+            formattedProjects += formatProjects[j][0].replace('%data%', projects.projects[k][formatProjects[j][1]]);
+        });
+        $.each(projects.projects[k]["images"], function(l, y) {
+            formattedProjects += HTMLprojectImage.replace('%data%', projects.projects[k]["images"][l]);
+        });
+        formattedProjects = formattedProjects.replace('#', projects.projects[k].url);
+        $('.project-entry').last().append(formattedProjects);
+    });
+}();
+
+education.display = function() {
+    var formatEduc = [
+        [HTMLschoolName, "name"],
+        [HTMLschoolDegree, "degree"],
+        [HTMLschoolDates, "dates"],
+        [HTMLschoolLocation, "location"]
+    ];
+    $.each(education.schools, function(k, v) {
+        var formattedEduc = '';
+        $('#education').append(HTMLschoolStart);
+        $.each(formatEduc, function(j, x) {
+            formattedEduc += formatEduc[j][0].replace('%data%', education.schools[k][formatEduc[j][1]]);
+        });
+        $.each(education.schools[k]["majors"], function(l, y) {
+            formattedEduc += HTMLschoolMajor.replace('%data%', education.schools[k]["majors"][l]);
+        });
+        formattedEduc = formattedEduc.replace('#', education.schools[k].url);
+        $('.education-entry').last().append(formattedEduc);
+    });
+    var formatOnline = [
+        [HTMLonlineTitle, "title"],
+        [HTMLonlineSchool, "school"],
+        [HTMLonlineDates, "dates"]
+    ];
+    $('#education').append(HTMLonlineClasses);
+    $.each(education.onlineCourses, function(k, v) {
+        var formattedOnline = '';
+        $('#education').append(HTMLschoolStart);
+        $.each(education.onlineCourses, function(j, x) {
+            formattedOnline += formatOnline[j][0].replace('%data%', education.onlineCourses[k][formatOnline[j][1]]);
+        });
+        formattedOnline = formattedOnline.replace('#', education.onlineCourses[k].url);
+        $('.education-entry').last().append(formattedOnline);
+    });
+}();
+
+work.display = function() {
+    var formatWork = [
+        [HTMLworkEmployer, "employer"],
+        [HTMLworkTitle, "title"],
+        [HTMLworkDates, "dates"],
+        [HTMLworkLocation, "location"],
+        [HTMLworkDescription, "description"]
+    ];
+    $.each(work.jobs, function(k, v) {
+        var formattedWork = '';
+        $('#workExperience').append(HTMLworkStart);
+        $.each(formatWork, function(j, x) {
+            formattedWork += formatWork[j][0].replace('%data%', work.jobs[k][formatWork[j][1]]);
+        });
+        formattedWork = formattedWork.replace('#', work.jobs[k].url);
+        $('.work-entry').last().append(formattedWork);
+    });
+}();
+
+$('#mapDiv').append(googleMap);
